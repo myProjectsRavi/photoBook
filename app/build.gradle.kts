2ea8b9a502921e673bc8d005e75391af99356a5e
myProjectsRavi <nekkalapuraviteja@gmail.com>
2026-06-20 03:39:38 -0700

    Increment version code to 10 and version name to 2.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.photobook.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "2.0.2"
+        versionCode = 10
+        versionName = "2.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
