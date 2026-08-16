61c6ba6bba1cbeb139f6b54873c713b7a422384b
myProjectsRavi <95069115+myProjectsRavi@users.noreply.github.com>
2026-08-16 00:10:47 -0700

    phase0: add Room schema plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
     id("org.jetbrains.kotlin.kapt") version "1.9.24" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
     id("androidx.baselineprofile") version "1.3.3" apply false
+    id("androidx.room") version "2.6.1" apply false
 }
